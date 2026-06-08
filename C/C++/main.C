418a48daea1756cdd99fa76d8e196f1e61da47d1
OttoMoraes <otto.moraes0404@gmail.com>
2026-06-08 01:12:47 +0000

    multi funções C language

--- C/C++/main.C
+++ C/C++/main.C
@@ -2,13 +2,20 @@
 
 int main() {
     printf("Deu certo!\n");
+
+    int a, b, soma, multi, divi;
     
-    int a, b, soma;
-    printf("escreva os numeros que quer somar:");
+    printf("escreva os numeros que quer calcular: ");
     scanf("%d %d", &a, &b);
+
+    
     soma = a + b;
-    printf("a soma desses numeroseh: %d", soma);
+    multi = a * b;
+    divi = a / b; 
+
+    printf("A soma eh: %d\n", soma);
+    printf("A multiplicacao eh: %d\n", multi);
+    printf("A divisao eh: %d\n", divi);
+
     return 0;
 }
-
-
