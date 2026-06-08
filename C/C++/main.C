23555e7940b0786367b95ebf76325c335c00fc20
OttoMoraes <otto.moraes0404@gmail.com>
2026-06-08 00:35:45 +0000

    commit

---
+++ C/C++/main.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Deu certo!\n");
+    
+    int a, b, soma;
+    printf("escreva os numeros que quer somar:");
+    scanf("%d %d", &a, &b);
+    soma = a + b;
+    printf("a soma desses numeroseh: %d", soma);
+    return 0;
+}
+
+
